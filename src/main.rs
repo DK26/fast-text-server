96e22ed828bfdd3daaf63edeb223a66e6ff557f9
David Krasnitsky <dikaveman@gmail.com>
2021-04-27 15:09:49 +0300

    Implemented TOML configurations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 mod base64;
+use std::{fs::File, io::Read};
+
+use serde_derive::Deserialize;
 
 use actix_web::{
     HttpServer,
@@ -9,14 +12,51 @@ use actix_web::{
 //     base64::test_hello();
 // }
 
+#[derive(Deserialize)]
+struct Config {
+    base64: Base64Config
+}
+
+#[derive(Deserialize)]
+struct Base64Config {
+    listen: String
+}
+
+fn init_cfg() -> Config {
+
+    let toml_path = "cfg.toml";
+
+    let mut file = File::open(&toml_path).expect("Unable to load `cfg.toml` file.");
+
+    let mut toml_contents= String::new();
+
+    file.read_to_string(&mut toml_contents).expect("Unable to load 'cfg.toml' contents.");
+
+    toml::from_str(&toml_contents).expect("Failed to parse 'cfg.toml'.")
+
+    // Config {
+    //     base64: Base64Config {
+    //         listen: "127.0.0.1:8080".to_owned()
+    //     }
+    // }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    
+    println!("Initializing service...");
+
+    let cfg = init_cfg();
+
+    println!("Initializing HTTP Server: {}", cfg.base64.listen);
+
     HttpServer::new(|| {
         App::new()
             .service(base64::welcome)
             .service(base64::echo)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(cfg.base64.listen)?
     .run()
     .await
+
 }
