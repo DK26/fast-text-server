2989996ccbd66a3ade6cb580fd6969bca38ad71d
David Krasnitsky <dikaveman@gmail.com>
2021-06-22 02:15:35 +0300

    Added TODOs + Advancement with Arguments

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,14 @@ use actix_web::{
 use simple_logger::SimpleLogger;
 use clap::{ArgMatches, Arg};
 
+// TODO: Create a const for each configuration default value, to make it consistent among all references.
+
 fn arg_matches<'a>() -> ArgMatches<'a> {
 
     let about = format!("Fast, lightweight RESTful API services for processing & modifying UTF-8 text messages.
     \nAuthor: {}\nSource: https://github.com/DK26/fast-webhooks", env!("CARGO_PKG_AUTHORS"));
  
+    // TODO: Continue the rest of the configurations
     clap::App::new("Fast-Webhooks")
         .version(env!("CARGO_PKG_VERSION"))
         .about(about.as_str())
@@ -38,8 +41,11 @@ lazy_static! {
 
     static ref CFG: Config = init_cfg();
 
+    static ref ARGS: ArgMatches<'static> = arg_matches();
+
     static ref PATTERNS_CACHE: RwLock<PatternsCache> = {
 
+        // TODO: Configurations to ARGS!
         let cache = PatternsCache::with_capacity(CFG.cache.regex_patterns_capacity)
             .limit(CFG.cache.regex_patterns_limit); 
 
@@ -55,7 +61,47 @@ pub const DEFAULT_ENCODING : &'static str = "utf-8";
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    let matches = arg_matches();
+    let cfg_bind = ARGS.value_of("listen")
+        .unwrap_or(&CFG.service.listen);
+
+    let cfg_server_hostname = ARGS.value_of("server_hostname")
+        .unwrap_or(&CFG.service.server_hostname);
+
+    let cfg_workers = match ARGS.value_of("workers") {
+        // Some(w) => w.parse().unwrap_or(CFG.service.workers),
+        Some(w) => w.parse().expect(&format!("Unable to parse '{}' as workers number.", w)),
+        None => CFG.service.workers
+    };
+
+    let cfg_backlog = match ARGS.value_of("backlog") {
+        Some(w) => w.parse().expect(&format!("Unable to parse '{}' as backlog number.", w)),
+        None => CFG.service.backlog
+    };
+
+    let cfg_max_connections = match ARGS.value_of("max_connections") {
+        Some(w) => w.parse().expect(&format!("Unable to parse '{}' as max_connections number.", w)),
+        None => CFG.service.max_connections
+    };
+
+    let cfg_keep_alive = match ARGS.value_of("keep_alive") {
+        Some(w) => w.parse().expect(&format!("Unable to parse '{}' as keep_alive number.", w)),
+        None => CFG.service.keep_alive
+    };
+
+    let cfg_client_timeout = match ARGS.value_of("client_timeout") {
+        Some(w) => w.parse().expect(&format!("Unable to parse '{}' as client_timeout number.", w)),
+        None => CFG.service.client_timeout
+    };
+
+    let cfg_client_shutdown = match ARGS.value_of("client_shutdown") {
+        Some(w) => w.parse().expect(&format!("Unable to parse '{}' as client_shutdown number.", w)),
+        None => CFG.service.client_shutdown
+    };
+
+    let cfg_shutdown_timeout = match ARGS.value_of("shutdown_timeout") {
+        Some(w) => w.parse().expect(&format!("Unable to parse '{}' as shutdown_timeout number.", w)),
+        None => CFG.service.shutdown_timeout
+    };
 
     SimpleLogger::new()
     .with_level(log::LevelFilter::Info)
@@ -76,15 +122,15 @@ async fn main() -> std::io::Result<()> {
             .service(services::decode_mime_subject)
             .service(services::regex_capture_group)
     })
-    .bind(&CFG.service.listen)?
-    .server_hostname(&CFG.service.server_hostname)
-    .workers(CFG.service.workers)
-    .backlog(CFG.service.backlog)
-    .max_connections(CFG.service.max_connections)
-    .keep_alive(CFG.service.keep_alive)
-    .client_timeout(CFG.service.client_timeout)
-    .client_shutdown(CFG.service.client_shutdown)
-    .shutdown_timeout(CFG.service.shutdown_timeout)
+    .bind(cfg_bind)?
+    .server_hostname(cfg_server_hostname)
+    .workers(cfg_workers)
+    .backlog(cfg_backlog)
+    .max_connections(cfg_max_connections)
+    .keep_alive(cfg_keep_alive)
+    .client_timeout(cfg_client_timeout)
+    .client_shutdown(cfg_client_shutdown)
+    .shutdown_timeout(cfg_shutdown_timeout)
     .run()
     .await
 }
